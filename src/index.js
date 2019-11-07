5c5453f907b2f93d02e3d0206ca520a64844fb82
wrogoz <wrogoz@gmail.com>
2019-11-07 13:57:02 +0100

    reading data excercise completed

--- src/index.js
+++ src/index.js
@@ -18,15 +18,71 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+            
+    }).catch(err=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/users',(req,res)=>{
+    User.find({ }).then((users)=>{
+        res.send(users)
+    }).catch((err)=>{
+        res.status(500).send()
+    })
+})
+
+
 app.post('/task', (req, res) => {
     const task = new Task(req.body)
    task.save().then(()=>{
        console.log('everything is ok')
-       res.send(task)
+       res.status(201).send(task)
    }).catch(()=>{
        console.log('dupa blada')
    })
 });
+
+app.get('/task', (req, res) => {
+    Task.find({}).then(tasks=>{
+        res.status(200).send(tasks)
+    }).catch(()=>{
+        res.status(500).send()
+    })
+});
+
+app.get('/task/:id', (req, res) => {
+    const _id = req.params.id;
+    Task.findById(_id).then((task)=>{
+        if(!task){
+          
+            return res.status(404).send();
+        }
+        res.send(task)
+    }).catch((err)=>{
+        res.status(500).send()
+    })
+});
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+            
+    }).catch(err=>{
+        res.status(500).send()
+    })
+})
+
 
 
 app.listen(port,()=>{
